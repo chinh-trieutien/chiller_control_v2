ff4898ecdfa623e5f89c8f4be30af25aa5668cd0
chinh-HomePC <chinh.trieutien@gmail.com>
2019-10-30 02:38:36 +0700

    add table object

--- dev/cc_chiller.py
+++ dev/cc_chiller.py
@@ -1,3 +1,16 @@
-class Chiller(object):
-    def __init__(self):
+class Table(object):
+    def __init__(self, data):
         pass
+
+
+class Chiller(object):
+    def __init__(self, table):
+        self.table = table
+
+
+if __name__ == '__main__':
+    tb1 = Table()
+    print(tb1)
+
+    ch1 = Chiller(tb1)
+    print(ch1)
